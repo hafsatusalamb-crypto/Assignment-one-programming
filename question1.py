94bb72d622cf170b271e48a9d32185de7a410151
hafsatusalamb-crypto <hafsatusalamb@icloud.com>
2026-05-12 18:37:47 -1200

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,6 @@
+# Customer Purchase Program
+
+customer_name = input("Enter customer name: ")
+item_purchased = input("Enter item purchased: ")
+
+print("Welcome,", customer_name + "!", "You have purchased a", item_purchased)
